46b9c82cb8545eee5e0e00a3c7373f1dd1a533d4
grahamutton <grahamu@gmail.com>
2015-09-17 07:10:39 +0100

    Remove redundant intial fork logic, improve output

--- analyse_branch.sh
+++ analyse_branch.sh
@@ -16,19 +16,17 @@ for upstream_br in master develop qa; do
     distance_to_merge_base_from_feature_br=$(git rev-list --first-parent $feature_br ^$merge_base --count)
     distance_to_merge_base_from_upstream_br=$(git rev-list --first-parent $upstream_br ^$merge_base --count)
 
-    if [ -z "$shortest_distance" ]; then
+    if [ -z "$shortest_distance" ] || [ "$distance_to_intial_fork_point" -lt "$shortest_distance" ]; then
       shortest_distance=$distance_to_intial_fork_point
       branch_with_most_recent_initial_fork=$upstream_br
-    else
-      if [ "$distance_to_intial_fork_point" -lt "$shortest_distance" ]; then
-        shortest_distance=$distance_to_intial_fork_point
-        branch_with_most_recent_initial_fork=$upstream_br
-      fi
     fi
 
-    echo -e "[$feature_br] diverged from an ancestor of [$upstream_br] $distance_to_intial_fork_point commits ago"
-    echo -e "distance to merge-base from [$feature_br] is $distance_to_merge_base_from_feature_br commits"
-    echo -e "distance to merge-base from [$upstream_br]'s $distance_to_merge_base_from_upstream_br commits\n"
+    echo -e "\n"
+    echo -e "[$feature_br]:"
+    echo -e "Was forked from an ancestor of [$upstream_br] $distance_to_intial_fork_point commits ago at initial_fork_point"
+    echo -e "Has most recent common ancestor (merge-base) $merge_base"
+    echo -e "Distance to merge-base from [$feature_br] is $distance_to_merge_base_from_feature_br commits"
+    echo -e "Distance to merge-base from [$upstream_br]'s $distance_to_merge_base_from_upstream_br commits\n"
 done
 
 echo "Forked from $branch_with_most_recent_initial_fork"
